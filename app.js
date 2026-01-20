f76d71acb75eaef0004a2172f963b4ecdfdbc210
Shivansh1608 <shivanshguptagla0508@gmail.com>
2026-01-20 15:15:57 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const stringUtils = require("./stringUtils");
-
-console.log(stringUtils.capitalize("nodejs"));    
-console.log(stringUtils.reverseString("backend"));  
-console.log(stringUtils.countVowels("Shivansh"));  
-
